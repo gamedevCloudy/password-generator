e4625aa3b571ee1cf5718a57220667db734ddb3f
gamedevCloudy <acaayush1111@gmail.com>
2023-01-07 21:30:20 +0530

    Update passwordgenerator.py
    -Fixed edge case: no letters
    -Bugs: Index Out of range for Unique and Numbers

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -8,23 +8,32 @@ def generate(length, hasCaps, hasSmall,hasNums, hasUnique, minNum,minUnique):
         for i in range(length): 
             x=r.randint(65,90)
             y=r.randint(97,122)
-            chance=[x,x,y,y,y] #60% small letter 40% capital
+            chance=[x,x,y,y,y] 
+            #60% small letter 40% capital
+            
             ch=r.randint(0,4)
             passwd[i]= chr(chance[ch])
+    
     elif hasCaps and not hasSmall: 
+        #onlycaps
         for i in range(length): 
             x=r.randint(65,90)
             passwd[i]= chr(x)
+    
     elif hasSmall and not hasCaps: 
+        #onlysmall
         for i in range(length): 
             x=r.randint(97,122)
             passwd[i]= chr(x)
+    else: 
+        print("No letters nigga? you should have combination of letters and numbers.")
+        exit(0)
     
 
     if(hasNums):
         minNum=minNum+r.randint(0,length//5)
         for i in range(minNum):
-            pos = r.randint(0,length)
+            pos = r.randint(0,length-1)
             passwd[pos]= str(r.randint(0,9))
     if(hasUnique):
         minUnique=minUnique+r.randint(0,length//5)
@@ -32,14 +41,17 @@ def generate(length, hasCaps, hasSmall,hasNums, hasUnique, minNum,minUnique):
 
         for i in range(minUnique):
             pos = r.randint(0,length-1)
+
             while(passwd[pos] in "0123456789"):
                 pos = r.randint(0,length-1)
-            passwd[pos]= unique[r.randint(0,8)]
+            passwd[pos]= unique[r.randint(0,7)]
 
-    print(passwd)
     password=''
     for i in passwd: 
         password+=i
     print(password)
     return password
-generate(14, True,True,True,True, 1,1)
+
+for i in range(10): 
+    generate(14, True,True,True,True, 1,1)
+    generate(14, False,False,True,True, 1,1)
